26ffc57dd26d966ad58324ade934786c7a7dd5bb
Adonis Kakoulidis <aklp08@gmail.com>
2021-01-02 04:44:08 +0200

    lets do some scheduling

--- bot.js
+++ bot.js
@@ -1,15 +1,37 @@
-const Discord = require('discord.js');
+const schedule = require('./schedule')
+const cron = require('node-cron')
+const Discord = require('discord.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log('I am ready!');
-});
+  console.log('I am ready!')
+  console.log("schedule:" + JSON.stringify(schedule, null, 2))
+
+  schedule.map(entry => {
+    cron.schedule(entry.cron, () => {
+      console.log('scheduling: ' + entry.label)
+
+      client.channels.cache.find(i => i.name === 'test').send(entry.label + "SOON")
+    }, {
+      scheduled: true,
+      timezone: "Europe/Amsterdam" // CET
+    })
+  })
+
+
+  // Editing a message the bot sent
+  // message.channel.send("Test").then(sentMessage => sentMessage.edit("Blah"))
+})
 
 client.on('message', message => {
   if (message.content === 'ping') {
-    message.reply('pong');
+    message.reply('pong')
   }
-});
+})
 
-client.login(process.env.BOT_TOKEN);
+client
+  .login(process.env.BOT_TOKEN)
+  .catch(e => {
+    console.log('CLIENT->LOGIN:ERROR: ' + e)
+  })
---
+++ schedule.js
@@ -0,0 +1,158 @@
+module.exports = [
+  {
+    label: "Kutum/Karanda",
+    cron: "15 0 * * MON"
+  },
+  {
+    label: "Kutum/Kzarka",
+    cron: "15 0 * * WED"
+  },
+  {
+    label: "Kzarka/Karanda",
+    cron: "15 0 * * FRI"
+  },
+  {
+    label: "Nouver/Kutum",
+    cron: "15 0 * * FRI"
+  },
+  {
+    label: "Karanda",
+    cron: "0 2 * * MON,WED"
+  },
+  {
+    label: "Kutum",
+    cron: "0 2 * * TUE,THU"
+  },
+  {
+    label: "Nouver",
+    cron: "0 2 * * FRI"
+  },
+  {
+    label: "Offin",
+    cron: "0 2 * * SAT"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 2 * * SUN"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 5 * * MON,TUE,WED"
+  },
+  {
+    label: "Nouver",
+    cron: "0 5 * * THU,SAT"
+  },
+  {
+    label: "Karanda",
+    cron: "0 5 * * FRI"
+  },
+  {
+    label: "Kutum",
+    cron: "0 5 * * SUN"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 9 * * MON"
+  },
+  {
+    label: "Nouver",
+    cron: "0 9 * * TUE,SUN"
+  },
+  {
+    label: "Karanda",
+    cron: "0 9 * * WED"
+  },
+  {
+    label: "Kutum",
+    cron: "0 9 * * THU,FRI,SAT"
+  },
+  {
+    label: "Offin",
+    cron: "0 12 * * MON"
+  },
+  {
+    label: "Kutum",
+    cron: "0 12 * * TUE"
+  },
+  {
+    label: "Nouver",
+    cron: "0 12 * * THU,SAT"
+  },
+  {
+    label: "Karanda",
+    cron: "0 12 * * FRI"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 12 * * SUN"
+  },
+  {
+    label: "Kutum",
+    cron: "0 16 * * MON"
+  },
+  {
+    label: "Nouver",
+    cron: "0 16 * * TUE,FRI"
+  },
+  {
+    label: "Kutum/Offin",
+    cron: "0 16 * * WED"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 16 * * THU"
+  },
+  {
+    label: "Quint/Muraka",
+    cron: "0 16 * * SAT"
+  },
+  {
+    label: "Vell",
+    cron: "0 16 * * SUN"
+  },
+  {
+    label: "Nouver",
+    cron: "0 19 * * MON"
+  },
+  {
+    label: "Karanda",
+    cron: "0 19 * * TUE"
+  },
+  {
+    label: "Vell",
+    cron: "0 19 * * WED"
+  },
+  {
+    label: "Kutum",
+    cron: "0 19 * * THU"
+  },
+  {
+    label: "Kzarka",
+    cron: "0 19 * * FRI"
+  },
+  {
+    label: "Kzarka/Karanda",
+    cron: "0 19 * * SAT"
+  },
+  {
+    label: "Garmoth",
+    cron: "0 19 * * SUN"
+  },
+  {
+    label: "Kzarka/Karanda",
+    cron: "15 22 * * WED"
+  },
+  {
+    label: "Quint/Muraka",
+    cron: "15 23 * * WED"
+  },
+  {
+    label: "Kutum/Kzarka",
+    cron: "15 22 * * FRI"
+  },
+  {
+    label: "Kzarka/Nouver",
+    cron: "15 22 * * SUN"
+  }
+]
